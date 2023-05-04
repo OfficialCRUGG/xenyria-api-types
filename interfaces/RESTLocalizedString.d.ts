eee8ec687a15a580b51e45fd369fdca1549ded1d
OfficialCRUGG <ich@crugg.de>
2023-05-04 21:07:56 +0200

    docs: RESTLocalizedString

--- interfaces/RESTLocalizedString.d.ts
+++ interfaces/RESTLocalizedString.d.ts
@@ -1,4 +1,27 @@
+/**
+ * Represents a localized string with both an english and a german version available.
+ *
+ * @example
+ * ```json
+ * {
+ *  "english": "Turf War",
+ *   "german": "Revierkampf"
+ * }
+ *
+ * @example
+ * ```typescript
+ * const localizedString: RESTLocalizedString = {
+ *  english: "Turf War",
+ *   german: "Revierkampf"
+ * };
+ */
 export interface RESTLocalizedString {
+  /**
+   * The english version of the string.
+   */
   english: string;
+  /**
+   * The german version of the string.
+   */
   german: string;
 }
